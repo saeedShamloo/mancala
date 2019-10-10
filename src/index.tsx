5ac1a69b399465646834aa73eba73e9061d0b34a
saeedShamloo <shamloo.1370@gmail.com>
2019-10-10 14:43:10 +0330

    update package.json

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import * as ReactDom from 'react-dom';
 import App from 'components/App';
 import 'antd/dist/antd.css';
 
-const a: string = 00;
 const element = (document as Document).getElementById('root');
 
 ReactDom.render(<App />, element);
